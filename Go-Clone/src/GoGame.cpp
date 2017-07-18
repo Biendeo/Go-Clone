8daa437ce0d0270487688de79c76fb106ea8c18c
Thomas Moffet <pantherman1996@gmail.com>
2017-07-18 21:54:32 +1000

    Made a SFML window right away.

--- Go-Clone/src/GoGame.cpp
+++ Go-Clone/src/GoGame.cpp
@@ -1,17 +1,22 @@
 #include "GoGame.h"
 
-#include "GameState.h"
+#include <iostream>
 
 
 GoGame::GoGame() {
+	//TODO: Arguments eventually.
+	//TODO: Maybe split up the video mode to see what you can put in it.
+	window = new sf::Window(sf::VideoMode(800, 600), "Go Game");
 }
 
 
 GoGame::~GoGame() {
+	delete window;
 }
 
 void GoGame::Start() {
-
+	//TODO: Create a game loop.
+	std::cout << "We're in the start function!\n";
 }
 
 const GameState& GoGame::GetGameState() const {
--- Go-Clone/src/GoGame.h
+++ Go-Clone/src/GoGame.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <SFML/Window.hpp>
+
+#include "GameState.h"
+
 /// <summary>
 /// The main class of the game. This should be made immediately in main, and arguments should be passed to it.
 /// </summary>
@@ -16,9 +20,12 @@ class GoGame {
 	/// <summary>
 	/// Returns the game state object for classes to refer to.
 	/// </summary>
-	const class GameState& GetGameState() const;
+	const GameState& GetGameState() const;
 
 	private:
 	GameState gameState;
+
+	//? Should I be using a smart pointer here?
+	sf::Window* window;
 };
 
