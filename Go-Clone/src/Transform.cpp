b791fb8b0e44935d43ffcae5200f08cc592d488f
Thomas Moffet <pantherman1996@gmail.com>
2017-07-27 14:34:43 +1000

    Added the GLM headers and implemented a Transform object.

--- Go-Clone/src/Transform.cpp
+++ Go-Clone/src/Transform.cpp
@@ -2,9 +2,25 @@
 
 
 
-Transform::Transform() {
+Transform::Transform(GameObject* gameObject) : Component(gameObject) {
+	translate = glm::vec3(0.0f, 0.0f, 0.0f);
+	rotate = glm::vec3(0.0f, 0.0f, 0.0f);
+	scale = glm::vec3(0.0f, 0.0f, 0.0f);
 }
 
 
 Transform::~Transform() {
+
+}
+
+glm::vec3& Transform::Translate() {
+	return translate;
+}
+
+glm::vec3& Transform::Rotate() {
+	return rotate;
+}
+
+glm::vec3& Transform::Scale() {
+	return scale;
 }
--- Go-Clone/src/Transform.h
+++ Go-Clone/src/Transform.h
@@ -1,7 +1,25 @@
 #pragma once
-class Transform {
+
+#include <glm/vec3.hpp>
+
+#include "Component.h"
+
+class Transform : public Component {
 	public:
-	Transform();
+	Transform(GameObject* gameObject);
 	~Transform();
+
+	glm::vec3& Translate();
+	glm::vec3& Rotate();
+	glm::vec3& Scale();
+
+	protected:
+	//TODO: I realised this doesn't work, redo this system.
+	static const bool unique = true;
+
+	private:
+	glm::vec3 translate;
+	glm::vec3 rotate;
+	glm::vec3 scale;
 };
 
